790a116aea9e2d65880587fce412710b010a3865
Abdul Rehman <abdulrehmanshafy101@gmail.com>
2025-12-17 16:42:03 +0500

    refactoring all the code , added a core app update the migrations

---
+++ trackforge/core/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+
+class AuditableModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # We use settings.AUTH_USER_MODEL for the custom user you made
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True
+    )
+
+    class Meta:
+        abstract = True # This tells Django: "Don't make a table for this, just lend the fields to others"
